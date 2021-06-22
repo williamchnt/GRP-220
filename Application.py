679ed63e8ae67162d51b1999e2f4a1737638353d
ArmandoLPollo <armanddrouineau92@gmail.com>
2021-06-22 10:24:03 +0200

    Ajout du logo pour l'import

--- Application.py
+++ Application.py
@@ -22,7 +22,7 @@ title = Label(appli, text = "Bienvenue sur Pr'eau'Pre", font = ("Ubisoft Sans Bo
 #ajout des boutons
 button1 = Button(detection_frame, text = "Detection photo", font = ("Ubisoft Sans Bold",20))
 button2 = Button(detection_frame, text = "Detection video", font = ("Ubisoft Sans Bold",20))
-button_import = Button(import_frame, text = "Importer photos/videos", font = ("Ubisoft Sans Bold",20))
+
 
 #ajout des images
 height = 220
@@ -31,6 +31,10 @@ width = 300
 logo = Image.open("logo_ocean_six_with_text.png")
 resized = logo.resize((300,225), Image.ANTIALIAS)
 final_logo = ImageTk.PhotoImage(resized)
+download_logo = Image.open("download_logo.png")
+rendu = ImageTk.PhotoImage(download_logo)
+
+button_import = Button(import_frame, text = "Importer photos/videos ", font = ("Ubisoft Sans Bold",20), image = rendu, compound = RIGHT)
 
 canvas = Canvas(appli, width = width, height = height, bg = "white", bd = 0, highlightthickness = 0)
 canvas.create_image(width/2, height/2, image = final_logo)
