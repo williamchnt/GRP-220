79de573e04d293f53985dd31882905d68a8c0860
williamchnt <56387759+williamchnt@users.noreply.github.com>
2021-06-30 11:36:15 +0200

    Class detect V1.0

--- detection/detect.py
+++ detection/detect.py
@@ -1,13 +1,11 @@
 class Detect:
-    cascade = "cascade-V0.1.xml"
+    cascade = "detection/cascade-V0.1.xml"
     
     
     def imageRead (self, image):
         import cv2
 
-        if(isinstance(image, str)):
-
-            
+        if(isinstance(image, str)):         
                 face_cascade=cv2.CascadeClassifier(self.cascade)
                 img = cv2.imread(image, cv2.IMREAD_COLOR)
 
@@ -24,25 +22,59 @@ class Detect:
         else:
             print("Erreur input")
 
-    def DetectAllImages (self, path):
+    def DetectAll (self, pathImport, pathResult):
         import os
 
-        entries = os.listdir('./ressources/import/img')
+        entries = os.listdir(pathImport)
 
         for entry in entries:
 
             extension = entry[-3]+entry[-2]+entry[-1]
             if((extension=="jpg")or(extension=="png")):
+                print("detecting : "+entry)
+                self.imageSave(entry,pathImport, pathResult)
 
-                self.imageSave(entry, path)
+            if((extension=="mp4")):
+                print("detecting : "+entry)
+                self.videoSave(entry, pathImport ,pathResult)
+
+        print("Program ending")
+
+    def DetectAllImage (self, pathImport, pathResult):
+        import os
+
+        entries = os.listdir(pathImport)
+
+        for entry in entries:
+
+            extension = entry[-3]+entry[-2]+entry[-1]
+            if((extension=="jpg")or(extension=="png")):
+                print("detecting : "+entry)
+                self.imageSave(entry,pathImport, pathResult)
+
+        print("Program ending")
+
+    def DetectAllVideo (self, pathImport, pathResult):
+        import os
+
+        entries = os.listdir(pathImport)
+
+        for entry in entries:
+
+            extension = entry[-3]+entry[-2]+entry[-1]
+
+            if((extension=="mp4")):
+                print("detecting : "+entry)
+                self.videoSave(entry, pathImport ,pathResult)
+
+        print("Program ending")
 
 
-    def imageSave (self, image, path):
+    def imageSave (self, image,pathImport, pathResult):
         import cv2
 
         if(isinstance(image, str)):
-                toDetect = "./ressourcesimport/img/"+image
-
+                toDetect = pathImport+"/"+image
             
                 face_cascade=cv2.CascadeClassifier(self.cascade)
                 img = cv2.imread(toDetect, cv2.IMREAD_COLOR)
@@ -52,54 +84,98 @@ class Detect:
                 for x, y, w, h in face:
                     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
                 
-                newTitle=path+"/"+image+"-detect.jpg"
-                print(newTitle)
+                newTitle=pathResult+"/"+image
                 cv2.imwrite(newTitle, img)
                 del img
 
         else:
             print("Erreur input")
 
-
-
     def video(self, video):
+        
+            import cv2
+
+            face_cascade=cv2.CascadeClassifier(self.cascade)
+            cap=cv2.VideoCapture(video)
+
+                # Check if camera opened successfully
+            if (cap.isOpened()== False): 
+                print("Error opening video stream or file")
+
+            # Read until video is completed
+            while(cap.isOpened()):
+            # Capture frame-by-frame
+                ret, frame=cap.read()
+                if ret == True:
+                    tickmark=cv2.getTickCount()
+                    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    face=face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3)
+                    for x, y, w, h in face:
+                        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                    if cv2.waitKey(1)&0xFF==ord('q'):
+                        break
+                    fps=cv2.getTickFrequency()/(cv2.getTickCount()-tickmark)
+                    cv2.putText(frame, "FPS: {:05.2f}".format(fps), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+
+                        # Display the resulting frame
+                    cv2.imshow('Frame',frame)
+
+                    # Press Q on keyboard to  exit
+                    if cv2.waitKey(1)&0xFF==ord('q'):
+                        break
+
+                    # Break the loop
+                else: 
+                    break
+
+            cap.release()
+            cv2.destroyAllWindows()
+
+    def videoSave(self, video,pathImport, pathResult):
 
         import cv2
 
         face_cascade=cv2.CascadeClassifier(self.cascade)
-        cap=cv2.VideoCapture(video)
+
+        toDetect = pathImport+"/"+video
+        cap=cv2.VideoCapture(toDetect)
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        fourcc=cv2.VideoWriter_fourcc(*'MP4V')
+        outputTitle=pathResult+"/"+video
+        out=cv2.VideoWriter(outputTitle,fourcc,20.0, (height,  width))
 
             # Check if camera opened successfully
         if (cap.isOpened()== False): 
             print("Error opening video stream or file")
 
+        frameCount = 1
         # Read until video is completed
         while(cap.isOpened()):
         # Capture frame-by-frame
             ret, frame=cap.read()
             if ret == True:
-                tickmark=cv2.getTickCount()
                 gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 face=face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3)
                 for x, y, w, h in face:
                     cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                if cv2.waitKey(1)&0xFF==ord('q'):
-                    break
-                fps=cv2.getTickFrequency()/(cv2.getTickCount()-tickmark)
-                cv2.putText(frame, "FPS: {:05.2f}".format(fps), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-
                     # Display the resulting frame
-                cv2.imshow('Frame',frame)
-
-                # Press Q on keyboard to  exit
-                if cv2.waitKey(1)&0xFF==ord('q'):
-                    break
+                out.write(frame)
+                pour = int((frameCount*100)/length)
+                print("Analyse "+video+" : "+str(pour)+"%")
+                frameCount+=1
+                #cv2.imshow('out', frame)
 
                 # Break the loop
+                if cv2.waitKey(1) == ord('q'):
+                    break
             else: 
                 break
 
         cap.release()
+        out.release()
         cv2.destroyAllWindows()
 
     def detectLive(self):
@@ -126,9 +202,5 @@ class Detect:
 
 
 model = Detect()
-#model.imageRead("test-image.jpg")
-#model.imageSave("plastique.jpg")
-model.DetectAllImages("C:/Users/Utilisateur/Documents/efrei/L3/mastercamp/git/GRP-220-2/ressources/result")
-#model.imageSave("test-image.jpg")
-#model.detectLive()
-#model.video("test.mp4")
+model.DetectAll("C:/Users/Utilisateur/Documents/efrei/L3/mastercamp/git/GRP-220-2/ressources/import/img", "C:/Users/Utilisateur/Documents/efrei/L3/mastercamp/git/GRP-220-2/ressources/result")
+#model.videoSave("test2.mp4","C:/Users/Utilisateur/Documents/efrei/L3/mastercamp/git/GRP-220-2/ressources/import/img", "C:/Users/Utilisateur/Documents/efrei/L3/mastercamp/git/GRP-220-2/ressources/result")
